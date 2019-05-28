b73fcaadcc3e0136fa8e4a6d0cadbe00c3e6b0b6
smatsuoka <mojaie@aol.com>
2019-05-29 00:20:06 +0900

    wip

--- deps/build.jl
+++ deps/build.jl
@@ -1,24 +1,32 @@
 
 using BinaryProvider
+using Libdl: dlext
 
-# Parse some basic command-line arguments
 const verbose = "--verbose" in ARGS
 const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "usr")))
 
 const url = "https://github.com/schrodinger/coordgenlibs/archive/v1.2.2.tar.gz"
-const hash = "8c2732785cf1f0c89830c81439dbfa59b6475f47"
-
-# These are the two binary objects we care about
+const hash = "e663e59c521e352d4994176b6826adf6c39a5b980291c26a68ea55f964bd88ef"
 products = Product[
     LibraryProduct(prefix, "libcoordgen", :libcoordgen)
 ]
 
-
-# First, check to see if we're all satisfied
 if any(!satisfied(p; verbose=verbose) for p in products)
-    # Download and install binaries
-    install(url, hash; prefix=prefix, force=true, verbose=true)
-
-    # Finally, write out a deps.jl file
-    write_deps_file(joinpath(@__DIR__, "deps.jl"), products)
+    tarball_dir = joinpath(prefix, "downloads")
+    tarball_path = joinpath(prefix, "downloads", "v1.2.2.tar.gz")
+    source_dir = joinpath(prefix, "downloads", "src")
+    install_dir = joinpath(source_dir, "build")
+    # delete old files
+    rm(tarball_dir, force=true, recursive=true)
+    rm(source_dir, force=true, recursive=true)
+    
+    verbose && @info("Unpacking $tarball_path into $source_dir")
+    download_verify(url, hash, tarball_path, force=true, verbose=verbose)
+    unpack(tarball_path, source_dir; verbose=verbose)
+    verbose && @info("Compiling in $source_dir...")
+    mkdir(install_dir)
+    cd(install_dir) do
+        run(`cmake ..`)
+    end
+    # write_deps_file(joinpath(@__DIR__, "deps.jl"), products, verbose=true)
 end
